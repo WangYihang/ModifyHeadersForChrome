89cd25a2d24b6a1627e80dac13b7db4471848b30
WangYihang <wangyihanger@gmail.com>
2017-01-03 18:15:57 +0800

    Just almost over

--- js/modifyUserAgent.js
+++ js/modifyUserAgent.js
@@ -1,26 +1,12 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
-  //console.log(JSON.stringify(details));
   var headers = details.requestHeaders,
   blockingResponse = {};
-
-  // Each header parameter is stored in an array. Since Chrome
-  // makes no guarantee about the contents/order of this array,
-  // you'll have to iterate through it to find for the
-  // 'User-Agent' element
-
-  var userHeaders = chrome.storage.local.get(null, function(results) {
+  chrome.storage.local.get(null, function(results) {
     for(var result in results){
       headers.push({"name":result, "value":results[result]});
     }
   });
-  for(var userHeader in userHeaders){
-    console.log(userHeader);
-    // headers.push({"name":"test", "value":"testValue"});
-  }
-
   blockingResponse.requestHeaders = headers;
-  // console.log(blockingResponse)
   return blockingResponse;
-
 },
 {urls: [ "<all_urls>" ]},['requestHeaders','blocking']);
