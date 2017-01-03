3e868ec74d94eea2936320185a8e28faa3b8ba2e
WangYihang <wangyihanger@gmail.com>
2017-01-03 18:15:17 +0800

    Just almost over

--- js/modifyUserAgent.js
+++ js/modifyUserAgent.js
@@ -7,23 +7,20 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
   // makes no guarantee about the contents/order of this array,
   // you'll have to iterate through it to find for the
   // 'User-Agent' element
-  console.log("aaa1")
 
-  chrome.storage.local.get(null, function(results) {
+  var userHeaders = chrome.storage.local.get(null, function(results) {
     for(var result in results){
-      console.log("获取 : ", result, " -> ", results[result]); 
       headers.push({"name":result, "value":results[result]});
     }
   });
-
-  console.log("aaa2")
-
-
-  for( var i = 0, l = headers.length; i < l; ++i ) {
-    console.log(headers[i].name, headers[i].value);
+  for(var userHeader in userHeaders){
+    console.log(userHeader);
+    // headers.push({"name":"test", "value":"testValue"});
   }
 
   blockingResponse.requestHeaders = headers;
+  // console.log(blockingResponse)
   return blockingResponse;
+
 },
 {urls: [ "<all_urls>" ]},['requestHeaders','blocking']);
